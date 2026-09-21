b84f44c97fa05f8676499f205387d60c5e18e62d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-09-21 00:31:03 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.trespaginasindividual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,7 @@
+// Export pages
+export '/pages/login/login_widget.dart' show LoginWidget;
+export '/pages/home_page1/home_page1_widget.dart' show HomePage1Widget;
+export '/pages/cadastro_email_nome/cadastro_email_nome_widget.dart'
+    show CadastroEmailNomeWidget;
+export '/pages/cadastr_endereco/cadastr_endereco_widget.dart'
+    show CadastrEnderecoWidget;
